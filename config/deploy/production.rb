54cdd35f8e00e8be17500ecf2e924fa83ef9b228
okada-takumi <okada@itpm-gk.com>
2018-01-06 11:15:45 +0000

    init unicorn2

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
 server '13.230.181.126', user: 'app', roles: %w{app db web}
 
-set :ssh_options, keys: '/Users/okada takumi/.ssh/id_rsa'
+set :ssh_options, keys: '/c/Users/okada takumi/.ssh/id_rsa'
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -15,15 +15,15 @@ CarrierWave.configure do |config|
 
   when 'production'
 
-    config.fog_directory = '本番環境用のバケット名'
+    config.fog_directory = 'achieve-produciton'
 
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/本番環境用のバケット名'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/achieve-produciton'
 
   when 'development'
 
-    config.fog_directory = '開発環境用のバケット名'
+    config.fog_directory = 'ach-development'
 
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/開発環境用のバケット名'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/ach-development'
 
   end
 
