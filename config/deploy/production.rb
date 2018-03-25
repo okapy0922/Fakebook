279bf055decf79337edd4e3cc4e32440a66eaf0b
okada-takumi <okada@itpm-gk.com>
2018-03-25 07:17:17 +0000

    add_task_DB_reset

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,14 @@
 server '54.248.40.32', user: 'app', roles: %w{app db web}
 set :ssh_options, keys: '/home/vagrant/.ssh/id_rsa'
+
+namespace :deploy do
+  task :db_reset do
+    on roles(:app) do
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          execute :rake, "db:migrate:reset"
+        end
+      end
+    end
+  end
+end
